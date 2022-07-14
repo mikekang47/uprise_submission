4e542ebd7983772936bf7563217b02c7cdcfd930
mikekang47 <gyeong0308@gmail.com>
2022-07-14 14:45:56 +0900

    Set environment

---
+++ src/main/java/com/example/uprise_submission/UpriseSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.uprise_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpriseSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpriseSubmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uprise_submission/UpriseSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uprise_submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpriseSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
